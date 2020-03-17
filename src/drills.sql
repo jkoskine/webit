5963ff8f8c9d88dd58d27bd87d28def7837650bc
Juha Koskinen <juhavkoskinen@hotmail.com>
2020-03-17 14:45:45 +0200

    More SQL

--- src/drills.sql
+++ src/drills.sql
@@ -3,3 +3,4 @@ FROM session_has_player
 JOIN player ON player.id_player = session_has_player.player_id_player
 JOIN session ON session.id_session = session_has_player.session_id_session
 JOIN drill ON drill.id_drill = session.drill_id_drill
+ORDER BY player.lname
